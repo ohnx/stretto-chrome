79bafb44e247d762b87c570d04c64e6596cc7a04
Mason X <masonx.01@gmail.com>
2016-10-22 19:24:10 -0400

    update some things
    v0.0.2

--- src/bg/background.js
+++ src/bg/background.js
@@ -16,7 +16,14 @@ chrome.commands.onCommand.addListener(function(command) {
             // if no tab found, open a new one
             if (tabs.length == 0) {
                 chrome.tabs.create({ url: items.remoteUrl });
+                if (command == 'playpause') {
+                    // by default, when the window opens, it is playing.
+                    // if we send a play/pause, it will pause it.
+                    // so here, we skip sending the playpause.
+                    return;
+                }
             }
+            // send request to remote
             var thing = dirJoin(items.remoteUrl, "command/"+items.remoteName+"/"+command);
             var xhr = new XMLHttpRequest();
             xhr.open("GET", thing, true);
@@ -39,4 +46,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
             }
         });
     });
+});
+
+chrome.runtime.onInstalled.addListener(function (object) {
+    chrome.runtime.openOptionsPage();
 });
